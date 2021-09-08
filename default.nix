78571a9b61913143d34a5b692691f52845a6dd18
ilkecan <ilkecan@protonmail.com>
2021-09-08 18:36:53 +0300

    Add ocaml-utils
    Added `getClosure`, `getOcamlPackages` and `getOcamlPackagesFrom`.

--- default.nix
+++ default.nix
@@ -2,19 +2,26 @@
 
 let
   inherit (builtins)
+    attrNames
     attrValues
     filter
     genericClosure
     isAttrs
     mapAttrs
     substring
+    tryEval
   ;
   inherit (lib)
     filterAttrs
     flatten
+    foldl
+    forEach
+    getAttrs
     hasPrefix
     hasSuffix
     makeSearchPath
+    mapAttrs'
+    nameValuePair
     optionals
     unique
   ;
@@ -67,12 +74,64 @@ let
       day = substring 6 2 lastModifiedDate;
     in
     "unstable-${year}-${month}-${day}";
+
+  # nix-instantiate --json --eval --expr 'with import <nixpkgs> {}; builtins.filter (lib.hasPrefix "ocamlPackages") (builtins.attrNames ocaml-ng)' | jq
+  ocamlScopeNames = [
+    "ocamlPackages"
+    "ocamlPackages_4_00_1"
+    "ocamlPackages_4_01_0"
+    "ocamlPackages_4_02"
+    "ocamlPackages_4_03"
+    "ocamlPackages_4_04"
+    "ocamlPackages_4_05"
+    "ocamlPackages_4_06"
+    "ocamlPackages_4_07"
+    "ocamlPackages_4_08"
+    "ocamlPackages_4_09"
+    "ocamlPackages_4_10"
+    "ocamlPackages_4_11"
+    "ocamlPackages_4_12"
+    "ocamlPackages_latest"
+  ];
+  mergeSets = sets: foldl (l: r: l // r) {} sets;
+
+  createOcamlOverlays = drvs: args:
+    mergeSets (forEach ocamlScopeNames (ocamlScopeName:
+      mapAttrs' (name: drv:
+        nameValuePair
+          "${ocamlScopeName}-${name}"
+          (final: prev: {
+            ocaml-ng = prev.ocaml-ng // {
+              ${ocamlScopeName} = prev.ocaml-ng.${ocamlScopeName}.overrideScope'
+                (final': prev': {
+                  ${name} = drv (final // { ocamlPackages = final'; } // args);
+                });
+            };
+          })
+      ) drvs
+    ));
+
+  filterBrokenPackages = packages:
+    filterAttrs (_: drv: (tryEval drv.outPath).success) packages;
+
+  getOcamlPackagesFrom = pkgs: packageNames: ocamlScopeName:
+    mapAttrs' (name: drv:
+      nameValuePair
+      "${ocamlScopeName}-${name}"
+      drv
+    ) (filterBrokenPackages (getAttrs packageNames pkgs.ocaml-ng.${ocamlScopeName}));
+
+  getOcamlPackages = pkgs: packageNames:
+    mergeSets (map (getOcamlPackagesFrom pkgs packageNames) ocamlScopeNames);
 in
 {
   inherit
-    getClosure
     createDebugSymbolsSearchPath
+    createOcamlOverlays
     createOverlays
+    getClosure
+    getOcamlPackages
+    getOcamlPackagesFrom
     getUnstableVersion
   ;
 }
