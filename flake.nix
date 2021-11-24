2bc85938faa14653a0fe49856c8d55377c706019
ilkecan <ilkecan@protonmail.com>
2021-11-24 13:40:29 +0300

    Add an overlay

--- flake.nix
+++ flake.nix
@@ -6,7 +6,12 @@
   };
 
   outputs = { self, nixpkgs }: {
+    overlay = final: prev: {
+      nix-utils = import ./lib { inherit (final) lib; };
+    };
+
     lib = import ./lib { inherit (nixpkgs) lib; };
+
     tests = import ./tests {
       inherit (nixpkgs) lib;
       nix-utils = self.lib;
