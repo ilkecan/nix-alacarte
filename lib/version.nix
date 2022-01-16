0d7916621cbfa61d23d08eacb1b0bc52588f95f6
ilkecan <ilkecan@protonmail.com>
2022-01-16 23:22:20 +0300

    Add getCmakeVersion

--- lib/version.nix
+++ lib/version.nix
@@ -2,10 +2,19 @@
 
 let
   inherit (builtins)
+    concatStringsSep
+    filter
+    lessThan
+    readFile
+    sort
     substring
   ;
   inherit (lib)
+    hasInfix
+    splitString
+    stringAsChars
   ;
+
   inherit (nix-utils)
   ;
 in
@@ -18,4 +27,17 @@ rec {
       day = substring 6 2 lastModifiedDate;
     in
     "unstable-${year}-${month}-${day}";
+
+  getCmakeVersion = file:
+    let
+      content = readFile file;
+      lines = splitString "\n" content;
+      versionLines = filter (hasInfix "_VERSION_") lines;
+      sortedVersionLines = sort lessThan versionLines;
+      isDigit = c: c >= "0" && c <= "9";
+      filterDigits = stringAsChars (c: if isDigit c then c else "");
+      versionNumbers = map filterDigits sortedVersionLines;
+      version = concatStringsSep "." versionNumbers;
+    in
+    version;
 }
--- tests/version.nix
+++ tests/version.nix
@@ -3,6 +3,7 @@
 let
   inherit (nix-utils)
     getUnstableVersion
+    getCmakeVersion
   ;
 in
 
@@ -11,5 +12,9 @@ in
     expr = getUnstableVersion "20211002221620";
     expected = "unstable-2021-10-02";
   };
-}
 
+  "getCmakeVersion" = {
+    expr = getCmakeVersion ./data/CMakeLists.txt;
+    expected = "2.7.3";
+  };
+}
