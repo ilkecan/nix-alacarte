5ef01a457f37704037d192c90e9d724189da9156
ilkecan <ilkecan@protonmail.com>
2022-07-29 13:19:28 +0000

    letterCase: introduce

--- lib/string.nix
+++ lib/string.nix
@@ -3,14 +3,33 @@
 let
   inherit (builtins)
     concatStringsSep
+    replaceStrings
   ;
 
   inherit (lib)
+    lowerChars
     splitString
+    upperChars
   ;
+
+  snakeSep = "_";
+  kebabSep = "-";
+  snakeChars = map (c: "${snakeSep}${c}") lowerChars;
+  kebabChars = map (c: "${kebabSep}${c}") lowerChars;
 in
 
 {
+  letterCase = {
+    camelToKebab = replaceStrings upperChars kebabChars;
+    camelToSnake = replaceStrings upperChars snakeChars;
+
+    kebabToCamel = replaceStrings kebabChars upperChars;
+    kebabToSnake = replaceStrings [ kebabSep ] [ snakeSep ];
+
+    snakeToCamel = replaceStrings snakeChars upperChars;
+    snakeToKebab = replaceStrings [ snakeSep ] [ kebabSep ];
+  };
+
   lines = splitString "\n";
 
   unlines = concatStringsSep "\n";
--- tests/string.nix
+++ tests/string.nix
@@ -5,9 +5,48 @@ let
     lines
     unlines
   ;
+
+  inherit (nix-utils.letterCase)
+    camelToKebab
+    camelToSnake
+    kebabToCamel
+    kebabToSnake
+    snakeToCamel
+    snakeToKebab
+  ;
 in
 
 {
+  "camelToKebab" = {
+    expr = camelToKebab "fooBar";
+    expected = "foo-bar";
+  };
+
+  "camelToSnake" = {
+    expr = camelToSnake "fooBar";
+    expected = "foo_bar";
+  };
+
+  "kebabToCamel" = {
+    expr = kebabToCamel "foo-bar";
+    expected = "fooBar";
+  };
+
+  "kebabToSnake" = {
+    expr = kebabToSnake "foo-bar";
+    expected = "foo_bar";
+  };
+
+  "snakeToCamel" = {
+    expr = snakeToCamel "foo_bar";
+    expected = "fooBar";
+  };
+
+  "snakeToKebab" = {
+    expr = snakeToKebab "foo_bar";
+    expected = "foo-bar";
+  };
+
   "lines_single" = {
     expr = lines "apple";
     expected = [ "apple" ];
