e7c742f239816b0efa615f0e3a685a40ac4edfa5
ilkecan <ilkecan@protonmail.com>
2022-07-31 19:34:53 +0000

    mkOverlay: introduce

--- lib/derivation.nix
+++ lib/derivation.nix
@@ -1,12 +1,30 @@
 { lib, nix-utils }:
 
 let
+  inherit (builtins)
+    baseNameOf
+  ;
+
+  inherit (lib)
+    removeSuffix
+  ;
+
   inherit (nix-utils)
     mergeListOfAttrs
+  ;
+
+  inherit (nix-utils.letterCase)
+    kebabToCamel
   ;
 in
 
 {
+  mkOverlay = inputs: drvFuncFile:
+    (final: prev: {
+      ${kebabToCamel (removeSuffix ".nix" (baseNameOf drvFuncFile))} =
+          final.callPackage drvFuncFile { inherit inputs; };
+    });
+
   overridePackageWith = pkg: overrides:
     pkg.override (mergeListOfAttrs overrides);
 
