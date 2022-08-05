746856b8159582ab277ff40bbdd2e2d205082a9b
ilkecan <ilkecan@protonmail.com>
2022-08-05 22:37:52 +0000

    flake: provide `libs` output

--- flake.nix
+++ flake.nix
@@ -6,9 +6,21 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      inherit (inputs.flake-utils.lib)
+        eachDefaultSystem
+        eachDefaultSystemMap
+      ;
+
+      inherit (nixpkgs.lib)
+        recursiveUpdate
+      ;
+    in
+    recursiveUpdate {
       overlays = rec {
         default = nix-utils;
+
         nix-utils = final: prev: {
           nix-utils = {
             lib = final.callPackage ./lib { };
@@ -18,7 +30,10 @@
       };
 
       lib = import ./lib { inherit (nixpkgs) lib; };
-    } // (inputs.flake-utils.lib.eachDefaultSystem (system:
+      libs = {
+        default = self.lib;
+      };
+    } (eachDefaultSystem (system:
       let
         pkgs = inputs.nixpkgs.legacyPackages.${system};
         inherit (pkgs) callPackage;
@@ -28,8 +43,11 @@
           nix-utils = { inherit (self) lib; };
         };
 
+        libs = recursiveUpdate self.lib self.pkgs-lib.${system};
+
         packages = rec {
           default = tests.lib;
+
           tests = {
             lib = callPackage ./tests/lib {
               nix-utils = { inherit (self) lib; };
