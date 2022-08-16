efbcd486319eebd31d4abe5cca0213baccb50dc2
ilkecan <ilkecan@protonmail.com>
2022-08-16 18:34:19 +0000

    isNull,notNull: add tests

--- tests/lib/misc.nix
+++ tests/lib/misc.nix
@@ -6,6 +6,8 @@
 let
   inherit (nix-utils)
     callWith
+    isNull
+    notNull
   ;
 in
 
@@ -13,5 +15,25 @@ in
   "callWith" = {
     expr = callWith 5 (n: n + 10);
     expected = 15;
+  };
+
+  "isNull_true" = {
+    expr = isNull null;
+    expected = true;
+  };
+
+  "isNull_false" = {
+    expr = isNull true;
+    expected = false;
+  };
+
+  "notNull_true" = {
+    expr = notNull 4.2;
+    expected = true;
+  };
+
+  "notNull_false" = {
+    expr = notNull null;
+    expected = false;
   };
 }
