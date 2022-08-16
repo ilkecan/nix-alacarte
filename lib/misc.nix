5f22a71c5f5b5ef5403f17230994d9aab2b9d0f8
ilkecan <ilkecan@protonmail.com>
2022-08-16 18:34:47 +0000

    Add `equals` and `notEquals`

--- lib/misc.nix
+++ lib/misc.nix
@@ -1,18 +1,28 @@
 {
-  lib,
   nix-utils,
   ...
 }:
+
+let
+  inherit (nix-utils)
+    equals
+    notEquals
+  ;
+in
 
 {
   callWith = val: f:
     f val;
 
-  isNull = value:
-    value == null;
+  equals = lhs: rhs:
+    lhs == rhs;
 
-  notNull = value:
-    value != null;
+  isNull = equals null;
+
+  notEquals = lhs: rhs:
+    lhs != rhs;
+
+  notNull = notEquals null;
 
   optionalValue = condition: value:
     if condition then
--- tests/lib/misc.nix
+++ tests/lib/misc.nix
@@ -6,7 +6,9 @@
 let
   inherit (nix-utils)
     callWith
+    equals
     isNull
+    notEquals
     notNull
   ;
 in
@@ -17,6 +19,16 @@ in
     expected = 15;
   };
 
+  "equals_true" = {
+    expr = equals 2 2;
+    expected = true;
+  };
+
+  "equals_false" = {
+    expr = equals "a" "b";
+    expected = false;
+  };
+
   "isNull_true" = {
     expr = isNull null;
     expected = true;
@@ -24,6 +36,16 @@ in
 
   "isNull_false" = {
     expr = isNull true;
+    expected = false;
+  };
+
+  "notEquals_true" = {
+    expr = notEquals [ 2 ] 2;
+    expected = true;
+  };
+
+  "notEquals_false" = {
+    expr = notEquals { v = 2; } { v = 2; };
     expected = false;
   };
 
