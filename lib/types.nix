ea765741317683fdef8d1e2bfce1b0d8cdbf67cb
ilkecan <ilkecan@protonmail.com>
2022-08-21 05:28:09 +0000

    types: add `coercibleToString`

--- lib/types.nix
+++ lib/types.nix
@@ -1,16 +1,62 @@
 {
   lib,
+  nix-utils,
   ...
 }:
 
 let
-  inherit (lib)
-    types
+  inherit (builtins)
+    hasAttr
+    head
+    typeOf
   ;
+
+  inherit (lib)
+    getValues
+    mergeEqualOption
+    mkOptionType
+  ;
+
+  inherit (lib.strings)
+    isCoercibleToString
+  ;
+
+  inherit (nix-utils)
+    allEqual
+    fmtValue
+  ;
+
+  types = lib.types // nix-utils.types;
 in
 
 {
   types = {
+    coercibleToString = coerceFunctions:
+      let
+        self = mkOptionType {
+          name = "coercibleToString";
+          check = x:
+            hasAttr (typeOf x) coerceFunctions || isCoercibleToString x;
+          merge = loc: defs:
+            let
+              values = getValues defs;
+              valueTypes = map typeOf values;
+              toStr = fmtValue coerceFunctions;
+            in
+            if allEqual valueTypes then
+              {
+                list = (types.listOf self).merge;
+                set = (types.attrsOf self).merge;  # maybe this should also be mergeEqualOption?
+              }.${head valueTypes} or mergeEqualOption loc defs
+            else
+              types.string.merge
+                loc
+                (map (def: def // { value = toStr def.value; }) defs)
+            ;
+        };
+      in
+      self;
+
     genericValue =
       let
         type = with types; oneOf [
