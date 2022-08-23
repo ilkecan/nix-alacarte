aba571c63ac0a58d56415420a821e8e71935a8a1
ilkecan <ilkecan@protonmail.com>
2022-08-23 07:20:36 +0000

    opennic: add `tlds`

---
+++ static/opennic.nix
@@ -0,0 +1,46 @@
+{
+  ...
+}:
+
+{
+  opennic = {
+    # https://wiki.opennic.org/opennic/dot
+    tlds = [
+      "bbs"
+      "chan"
+      "cyb"
+      "dyn"
+      "epic"
+      "geek"
+      "gopher"
+      "indy"
+      "libre"
+      "neo"
+      "null"
+      "o"
+      "oss"
+      "oz"
+      "parody"
+      "pirate"
+
+      # inactive
+      "free"
+
+      # peered
+      "bazar"
+      "coin"
+      "emc"
+      "lib"
+      "fur"
+      "ku"
+      "te"
+      "ti"
+      "uu"
+      "ko"
+      "rm"
+
+      # technical
+      "glue"
+    ];
+  };
+}
