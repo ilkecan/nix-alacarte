5b89b13b99cd6e25da775f71f30c08d4ca25b35a
ilkecan <ilkecan@protonmail.com>
2022-08-28 18:15:07 +0000

    Remove `concatFiles` in favor of `pkgs.concatText`

--- src/pkgs-lib/file.nix
+++ src/pkgs-lib/file.nix
@@ -1,18 +1,13 @@
 {
   inputs,
   system,
-  nix-utils,
   pkgs ? inputs.nixpkgs.legacyPackages.${system},
   ...
 }:
 
 let
-  inherit (nix-utils)
-    concatFiles
-  ;
-
   inherit (pkgs)
-    runCommandLocal
+    concatText
     writeTextFile
   ;
 
@@ -22,29 +17,13 @@ let
         name = "temp";
         inherit text;
       };
-    in
-    concatFiles {
       name = baseNameOf file;
       files = if append then [ file tempFile ] else [ tempFile file ];
-    };
+    in
+    concatText name files;
 in
 
 {
-  concatFiles =
-    {
-      files,
-      name ? "concatenated-files",
-      ...
-    }@args:
-    let
-      env = args // {
-        passAsFile = [ "files" ];
-      };
-    in
-    runCommandLocal name env ''
-      cat $(cat $filesPath) > $out
-    '';
-
   appendToFile = addToFile true;
   prependToFile = addToFile false;
 }
