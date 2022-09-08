9b1a46d14fc03d381db7f214a1bf45426784ca9d
ilkecan <ilkecan@protonmail.com>
2022-09-08 01:07:40 +0000

    Add `patchInputs`

---
+++ src/lib/flake.nix
@@ -0,0 +1,65 @@
+{
+  inputs,
+
+  lib,
+  nix-alacarte,
+  ...
+}:
+
+let
+  inherit (builtins)
+    attrValues
+    mapAttrs
+  ;
+
+  inherit (lib)
+    filterAttrs
+    genAttrs
+    pipe
+  ;
+
+  inherit (nix-alacarte)
+    dirToAttrs
+    forEachAttr
+    notEmpty
+  ;
+in
+
+{
+  patchInputs = inputs': patchesDir: systems:
+    let
+      patchesFor = pipe patchesDir [
+        dirToAttrs
+        (mapAttrs (_: attrValues))
+        (filterAttrs (_: notEmpty))
+      ];
+    in
+    genAttrs systems (system:
+      let
+        inherit (inputs.nixpkgs.legacyPackages.${system}) applyPatches;
+      in
+      inputs' // forEachAttr patchesFor (inputName: patches:
+        let
+          input = inputs'.${inputName};
+          # TODO: use fetchTree after https://github.com/NixOS/nix/pull/6530 is merged
+          # args = flakeLock.nodes.${inputName}.original // {
+          #   inherit patches;
+          # };
+          # patchedInput = builtins.fetchTree args;
+          patchedInput = applyPatches {
+            name = inputName;
+            src = input;
+            inherit patches;
+          };
+          flake = import "${patchedInput}/flake.nix";
+          self = flake.outputs inputs;
+          inputs = { inherit self; } // input.inputs;
+          sourceInfo = input.sourceInfo // { inherit (patchedInput) outPath; };
+        in
+        self // sourceInfo // {
+          outputs = self;
+          inherit inputs sourceInfo;
+        }
+      )
+    );
+}
