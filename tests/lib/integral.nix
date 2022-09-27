c159ce7e71cb954be280828b0e618f26bcc1ee1d
ilkecan <ilkecan@protonmail.com>
2022-09-27 15:30:26 +0000

    {increment,decrement}: add tests using floats

--- tests/lib/integral.nix
+++ tests/lib/integral.nix
@@ -16,13 +16,29 @@ let
 in
 
 {
-  increment = assertEqual {
-    actual = increment 12;
-    expected = 13;
+  decrement = {
+    integer = assertEqual {
+      actual = decrement 23;
+      expected = 22;
+    };
+
+    float = assertEqual {
+      actual = decrement 24.23;
+      expected = 23.23;
+    };
   };
 
-  decrement = assertEqual {
-    actual = decrement 23;
-    expected = 22;
+  increment = {
+    integer = assertEqual {
+      actual = increment 12;
+      expected = 13;
+    };
+
+    float = assertEqual {
+      actual = increment 12.74;
+      expected = 13.74;
+    };
+  };
+
   };
 }
