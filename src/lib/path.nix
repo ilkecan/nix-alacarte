b74499f93c5b5cd106ee2026ea5c9d9d01a071d0
ilkecan <ilkecan@protonmail.com>
2022-09-29 17:20:34 +0000

    Add `pathComponents`

--- src/lib/path.nix
+++ src/lib/path.nix
@@ -1,14 +1,38 @@
 {
+  lib,
+  nix-alacarte,
   ...
 }:
 
 let
   inherit (builtins)
+    elem
     substring
+  ;
+
+  inherit (lib)
+    splitString
+  ;
+
+  inherit (nix-alacarte)
+    compose
+    equalTo
+    ifilter
+    imap
+    pipe'
   ;
 in
   
 {
-  isAbsolutePath = path:
-    substring 0 1 path == "/";
+  isAbsolutePath = compose [ (equalTo "/") (substring 0 1) ];
+
+  pathComponents =
+    let
+      componentsToRemove = [ "." "" ];
+    in
+    pipe' [
+      (splitString "/")
+      (ifilter (i: v: !elem v componentsToRemove || i == 0))
+      (imap (i: v: if i == 0 && v == "" then "/" else v))
+    ];
 }
--- tests/lib/path.nix
+++ tests/lib/path.nix
@@ -7,9 +7,11 @@
 let
   inherit (nix-alacarte)
     isAbsolutePath
+    pathComponents
   ;
 
   inherit (dnm)
+    assertEqual
     assertFalse
     assertTrue
   ;
@@ -20,5 +22,47 @@ in
     path = assertTrue isAbsolutePath ./fixtures/example-project;
     string_absolute = assertTrue isAbsolutePath "/var/root";
     string_relative = assertFalse isAbsolutePath ".git/config";
+  };
+
+  pathComponents = {
+    relative_path = assertEqual {
+      actual = pathComponents "a/b";
+      expected = [ "a" "b" ];
+    };
+
+    repeated_separator = assertEqual {
+      actual = pathComponents "a//b";
+      expected = [ "a" "b" ];
+    };
+
+    dot_in_the_middle = assertEqual {
+      actual = pathComponents "a/./b";
+      expected = [ "a" "b" ];
+    };
+
+    separator_at_the_end = assertEqual {
+      actual = pathComponents "a/b/";
+      expected = [ "a" "b" ];
+    };
+
+    dot_at_the_end = assertEqual {
+      actual = pathComponents "a/b/.";
+      expected = [ "a" "b" ];
+    };
+
+    dot_at_the_beginning = assertEqual {
+      actual = pathComponents "./a/b";
+      expected = [ "." "a" "b" ];
+    };
+
+    separator_at_the_beginning = assertEqual {
+      actual = pathComponents "/a/b";
+      expected = [ "/" "a" "b" ];
+    };
+
+    double_dot_is_kept = assertEqual {
+      actual = pathComponents "a/b/../c";
+      expected = [ "a" "b" ".." "c" ];
+    };
   };
 }
