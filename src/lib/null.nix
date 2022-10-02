4846908490458e922b43a831e8e9aa72785c3102
ilkecan <ilkecan@protonmail.com>
2022-10-02 22:24:25 +0000

    Add `unwrapOr`

--- src/lib/null.nix
+++ src/lib/null.nix
@@ -12,5 +12,11 @@ in
 
 {
   isNull = equalTo null;
+
   notNull = notEqualTo null;
+
+  unwrapOr = default: value:
+    if value == null
+      then default
+      else value;
 }
--- tests/lib/null.nix
+++ tests/lib/null.nix
@@ -8,9 +8,11 @@ let
   inherit (nix-alacarte)
     isNull
     notNull
+    unwrapOr
   ;
 
   inherit (dnm)
+    assertEqual
     assertFalse
     assertTrue
   ;
@@ -26,4 +28,20 @@ in
     not_null = assertTrue notNull 4.2;
     null = assertFalse notNull null;
   };
+
+  unwrapOr =
+    let
+      unwrapOr' = unwrapOr 24;
+    in
+    {
+      not_null = assertEqual {
+        actual = unwrapOr' 2;
+        expected = 2;
+      };
+
+      null = assertEqual {
+        actual = unwrapOr' null;
+        expected = 24;
+      };
+    };
 }
